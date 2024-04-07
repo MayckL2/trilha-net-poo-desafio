5648237c3c636fd4bc77dd3b531dd2ed748e7cb2
mayckl2 <mayckluciano2@gmail.com>
2024-04-07 11:26:16 -0300

    feat: Conclusão do desafio de POO

--- Models/Nokia.cs
+++ Models/Nokia.cs
@@ -1,8 +1,16 @@
 namespace DesafioPOO.Models
 {
     // TODO: Herdar da classe "Smartphone"
-    public class Nokia
+    public class Nokia : Smartphone
     {
+        public Nokia(string numero, string modelo, string imei, int memoria) :base(numero, modelo, imei, memoria){
+            Console.WriteLine($"Fabricando {modelo} n°{imei}, {memoria}GB de memoria e numero {numero}.");
+        }
+
         // TODO: Sobrescrever o método "InstalarAplicativo"
+        public override void InstalarAplicativo(string nomeApp){
+            Console.WriteLine($"Instalando {nomeApp}...");
+            Console.WriteLine($"{nomeApp} Instalado com suceso!");
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,3 +1,12 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Iphone iphone11 = new("11111111", "iphone11", "123456", 50);
+iphone11.Ligar();
+iphone11.ReceberLigacao();
+iphone11.InstalarAplicativo("zapzap");
+
+Nokia nokia = new("22222222", "nokia", "123456", 50);
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("zapzap");
